cdde669be3c865819e17da0aec7a2681bf14b0a9
Wolf <wolf@tgwolf.com>
2024-06-24 18:25:13 +0100

    Add capabilities to support multiple requirements files

--- pipeline.sh
+++ pipeline.sh
@@ -292,7 +292,7 @@ function install_prerequisites()
                 fail "${CMD[*]}" "${errors}" true
                 exit "${EXIT_VALUE}"
             fi
-        done < <(find . -name 'requirements.txt' -type f -not -path "./.git/*" | sed 's|^./||' | sort -Vf || true)
+        done < <(find . -name 'requirements*.txt' -type f -not -path "./.git/*" | sed 's|^./||' | sort -Vf || true)
     fi
 }
 
